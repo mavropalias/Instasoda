e03b3159a858bc9fd7768522908ced1501e63858
Kostas Mavropalias <mavropalias@kostas.(none)>
2011-12-20 20:55:23 +0000

    Windows 8 app - initial upload

---
+++ apps/win8/Instasoda/js/homePage.js
@@ -0,0 +1,95 @@
+﻿(function () {
+    'use strict';
+
+    // Custom event raised after the fragment is appended to the DOM.
+    WinJS.Application.addEventListener('fragmentappended', function handler(e) {
+        if (e.location === '/html/homePage.html') { fragmentLoad(e.fragment, e.state); }
+    });
+
+    function fragmentLoad(elements, options) {
+        WinJS.UI.processAll(elements)
+            .then(function () {
+                // TODO: Initialize the fragment here.
+            });
+    }
+
+    WinJS.Namespace.define('homePage', {
+        fragmentLoad: fragmentLoad,
+    });
+})();
+
+
+$(document).ready(function () {
+
+    if (IS.isLoggedIn()) {
+        $('.homePage').hide();
+
+        //TODO: goto dashboard
+    }
+
+    var iPackage = 0;
+    var scope1 = "email,user_relationships,user_location,user_hometown,user_birthday";
+    var scope2 = scope1 + ",user_activities,user_education_history,read_stream,user_interests,user_likes,user_photos";
+    var scope3 = scope2 + ",sms,offline_access,user_videos";
+
+    $('#register1').click(function () {
+        iPackage = 1;
+        launchFacebookWebAuth(scope1);
+    });
+    $('#register2').click(function () {
+        iPackage = 2;
+        launchFacebookWebAuth(scope2);
+    });
+    $('#register3').click(function () {
+        iPackage = 3;
+        launchFacebookWebAuth(scope3);
+    });
+
+    function launchFacebookWebAuth(scope) {
+        var facebookURL = "https://www.facebook.com/dialog/oauth?client_id=";
+        var clientID = "159444727449256";
+        var callbackURL = "https://www.facebook.com/connect/login_success.html";
+        facebookURL += clientID + "&redirect_uri=" + encodeURIComponent(callbackURL) +
+ "&scope=" + scope +"&display=popup&response_type=token";
+
+        try {
+            var startURI = new Windows.Foundation.Uri(facebookURL);
+            var endURI = new Windows.Foundation.Uri(callbackURL);
+
+            Windows.Security.Authentication.Web.WebAuthenticationBroker.authenticateAsync(
+			Windows.Security.Authentication.Web.WebAuthenticationOptions.default,
+			startURI,
+			endURI).then(callbackFacebookWebAuth, callbackFacebookWebAuthError);
+        }
+        catch (err) {	/*Error launching WebAuth"*/	return; }
+    }
+
+    function callbackFacebookWebAuth(result) {
+        var FacebookReturnedToken = result.responseData;
+        var response = "Status returned by WebAuth broker: " + result.responseStatus + "\r\n";
+        if (result.responseStatus == 2) {
+            response += "Error returned: " + result.responseErrorDetail + "\r\n";
+            $('#fbResponse').html("<strong>Oh snap! An error occured :( Try again please.</strong>");
+        } else if (result.responseStatus == 0) {
+            //success
+            //IS.createAccount(iPackage, FacebookReturnedToken);
+            sToken = getParameterByName("access_token", FacebookReturnedToken);
+            $('#fbResponse').html(FacebookReturnedToken + " <br><br> >>> " + sToken);
+        }
+    }
+
+    function callbackFacebookWebAuthError(err) {
+        var error = "Error returned by WebAuth broker. Error Number: " + err.number + " Error Message: " + err.message + "\r\n";
+    }
+
+    function getParameterByName(sParam, FacebookReturnedToken) {
+
+        var match = RegExp('[#&]' + sParam + '=([^&]*)')
+                    .exec(FacebookReturnedToken);
+
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+
+    }
+
+
+});
---
+++ apps/win8/Instasoda/js/instasoda.js
@@ -0,0 +1,199 @@
+var IS = new function () {
+
+    // Private variables
+    var username = '';
+    var apiKey = 'af98y34ubliuyasv097hb34uig';
+    var isLoggedIn = false;
+    var activityFeedPage = 0;
+
+    // Public variables
+    // this.publicVar = 'public';
+
+    // ***************************************************
+    // Private methods
+    // ***************************************************
+
+    var testMethod = function () {
+
+    }
+
+    // ***************************************************
+    // Public methods
+    // ***************************************************
+
+    /**
+    * Create a new user account by connecting to Facebook.
+    * @param {Integer} package 1=basic, 2=standard, 3=complete
+    * @param {String} facebookToken
+    * @return {Object} Returns an object {'s': 'success/fail'}
+    */
+    this.createAccount = function (iPackage, sFacebookToken) {
+
+    }
+
+    /**
+    * Logs the user in the system.
+    * @param {String} username
+    * @param {String} password
+    * @param {Function} [callback]
+    * @return {Object} Returns an object {'s': 'success/fail'}
+    */
+    this.login = function (sUsername, sPassword, sCallback) {
+        username = sUsername;
+        isLoggedIn = true;
+        if (sCallback) {
+            sCallback({ 's': 'success' });
+        } else {
+            return { 's': 'success' };
+        }
+    }
+
+    /**
+    * Logs the user out of the system.
+    * @param {Function} [callback]
+    * @return {Object}	Returns an object {'s': 'success/fail'}
+    */
+    this.logout = function (sCallback) {
+        isLoggedIn = false;
+
+        if (sCallback) {
+            sCallback({ 's': 'success' });
+        } else {
+            return { 's': 'success' };
+        }
+    }
+
+    /**
+	* Returns the login status of the user.
+	* @return {Bool}	If the user is logged-in it returns true, otherwise false.
+	*/
+    this.isLoggedIn = function () {
+        return isLoggedIn;
+
+    }
+
+    /**
+	* Returns the username of the currently logged-in user.
+	* @return {String}	The username.
+	*/
+    this.username = function () {
+        return username;
+    }
+
+    /**
+	* Returns the user's notifications.
+	* @param {Object}	[callback]
+	* @return {Object}
+	*/
+    this.getNotifications = function (sCallback) {
+        var jNotifications =
+        {
+            "notifications":
+	                  [
+            {
+                "date": "3 hours ago",
+                "title": "Jill, 29",
+                "message": "She joined Instasoda near you.",
+                "image": "/api/images/1.png",
+                "compatibility": 47
+            },
+            {
+                "date": "4 hours ago",
+                "title": "Kate, 26",
+                "message": "She checked you out a few hours ago.",
+                "image": "/api/images/2.png",
+                "compatibility": 68
+            },
+            {
+                "date": "12 hours ago",
+                "title": "Nancy, 27",
+                "message": "She wants to meet you.",
+                "image": "/api/images/3.png",
+                "compatibility": 73
+            },
+            {
+                "date": "15 hours ago",
+                "title": "Dianna, 28",
+                "message": "She joined Instasoda near you.",
+                "image": "/api/images/4.png",
+                "compatibility": 92
+            }
+	                  ]
+        };
+
+        if (sCallback) {
+            sCallback(jNotifications);
+        } else {
+            return jNotifications;
+        }
+    }
+
+    /**
+	* Returns the user's activity feed (30 items at a time).
+	* @param (Bool)	[more]	When true, returns the next 30 items.
+	* @param {Object}	[callback]
+	* @return {Object}
+	*/
+    this.getActivityFeed = function (bMore, sCallback) {
+        activityFeedPage = 1;
+        var jActivity =
+        {
+            "recent_updates":
+	                  [
+            {
+                "date": "3 hours ago",
+                "title": "Jill, 29",
+                "message": "She joined Instasoda near you.",
+                "image": "/api/images/1.png",
+                "compatibility": 47
+            },
+            {
+                "date": "4 hours ago",
+                "title": "Kate, 26",
+                "message": "She checked you out a few hours ago.",
+                "image": "/api/images/2.png",
+                "compatibility": 68
+            },
+            {
+                "date": "12 hours ago",
+                "title": "Nancy, 27",
+                "message": "She wants to meet you.",
+                "image": "/api/images/3.png",
+                "compatibility": 73
+            },
+            {
+                "date": "15 hours ago",
+                "title": "Dianna, 28",
+                "message": "She joined Instasoda near you.",
+                "image": "/api/images/4.png",
+                "compatibility": 92
+            }
+	                  ]
+        };
+
+        if (sCallback) {
+            sCallback(jActivity);
+        } else {
+            return jActivity;
+        }
+    }
+
+    /**
+	* Returns an array of users.
+	* @param (JSON)	options	Options to customise the user array
+	* @param {Object}	[callback]
+	* @return {Object}
+	*/
+    this.getUsers = function (iNumUsers, jOptions, sCallback) {
+
+        //TODO
+
+
+        if (sCallback) {
+            sCallback(jUsers);
+        } else {
+            return jUsers;
+        }
+    }
+
+}
