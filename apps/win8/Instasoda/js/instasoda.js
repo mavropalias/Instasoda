6cbeb65d8c7a3d768655f4016edfe131fdb3446a
Kostas Mavropalias <mavropalias@kostas.(none)>
2011-12-24 17:35:21 +0000

    added models + views

--- apps/win8/Instasoda/js/instasoda.js
+++ apps/win8/Instasoda/js/instasoda.js
@@ -2,6 +2,8 @@
 // ===================================================
 // = Instasoda client library
 // = > requires jQuery 1.7+
+// = > requires Backbone.js
+// = > requires Mustache.js
 // ===================================================
 // ===================================================
 
@@ -13,18 +15,67 @@ var IS = new function () {
     // ===================================================
     // ===================================================
 
-    jQuery.support.cors = true;
+        var sApi = "http://www.instasoda.com/api/";
+        jQuery.support.cors = true;
+        Backbone.emulateHTTP = true;
+
+
+    // ===================================================
+    // ===================================================
+    // = Models
+    // ===================================================
+    // ===================================================
+
+        var User = Backbone.Model.extend({
+            defaults: {
+                username: 'new user'
+            },
+            url: sApi + 'user.php',
+        });
+
+
+    // ===================================================
+    // ===================================================
+    // = Views
+    // ===================================================
+    // ===================================================
+
+        var UserSettingsView = Backbone.View.extend({
+            tagName: 'div',
+            id: 'userSettingsView2',
+            initialize: function(options) {
+                _.bindAll(this, 'render');
+                this.model.bind('change', this.render);
+                this.render();
+            },
+            render: function () {
+                //TODO: render view template
+                //$(this.el).html(this.template(this.model.toJSON()));
+                //$('#userSettingsView').html(this.model.toJSON());
+                //$(this.el).html(this.model.get('username'));
+                this.el.innerHTML = this.model.get('username');
+                $('#userSettingsView').html(this.el);
+                
+            }
+        });
 
     // ===================================================
     // ===================================================
     // = Private variables
     // ===================================================
     // ===================================================
+        
+        var username = "";
+        var isLoggedIn = false;
+        var activityFeedPage = 0;
 
-    var sApi = "http://www.instasoda.com/api/";
-    var username = "";
-    var isLoggedIn = false;
-    var activityFeedPage = 0;
+        // models
+        var user = new User();
+
+        // views
+        var userSettingsView = new UserSettingsView({
+            model: user,
+        });
 
 
     // ===================================================
@@ -33,7 +84,40 @@ var IS = new function () {
     // ===================================================
     // ===================================================
 
-    var testMethod = function () {}
+        /**
+        * Store data locally, using one of the following methods:
+        * 1. localStorage
+        * 2. local DB
+        * 3. Cookies
+        * @param {String} varName
+        * @param {Object} data 
+        * @return {Bool} success
+        */
+        var saveLocally = function (sVarName, jData) {
+            if (!!window.localStorage) {
+                window.localStorage.sVarName = JSON.stringify(jData);
+                return true;
+            } else {
+                //TODO: alternative storage solution, when localStorage is not available
+                throw("Local storage is not available");
+            }
+        }
+
+        /**
+        * Update data that have been saved locally.
+        * @param {String} varName
+        * @param {Object} data
+        * @return {Bool} success
+        */
+        var updateLocally = function (sVarName, jData) {
+            if (!!window.localStorage) {
+                window.localStorage.sVarName = JSON.stringify(jData);
+                return true;
+            } else {
+                //TODO: alternative storage solution, when localStorage is not available
+                throw("Local storage is not available");
+            }
+        }
 
 
     // ===================================================
@@ -42,218 +126,94 @@ var IS = new function () {
     // ===================================================
     // ===================================================
 
-    /**
-    * Create a new user account by connecting to Facebook.
-    * @param {Integer} package 1=basic, 2=standard, 3=complete
-    * @param {String} facebookToken
-    * @param {Function} callback
-    * @return {Object} Returns an object {'s': 'success/fail'}
-    */
-    this.createAccount = function (iPackage, sFacebookToken, callback) {
+        /**
+        * Create a new user account by connecting to Facebook.
+        * @param {Integer} package 1=basic, 2=standard, 3=complete
+        * @param {String} facebookToken
+        * @param {Function} callback
+        * @return {Object} Returns an object {'s': 'success/fail'}
+        */
+        this.createAccount = function (iPackage, sFacebookToken, callback) {
 
-        var user = {
-            'isPackage': iPackage,
-            'fbToken': sFacebookToken
-        };
-
-        // store the user details locally
-        if (!!window.localStorage) {
-            window.localStorage.user = JSON.stringify(user);
-        } else {
-            //TODO: alternative storage solution, when localStorage is not available
-            callback(false, "Local storage is not available");
-        }
-
-        // connect to the API server and create the account
-        $.ajax({
-            type: 'GET',
-            url: sApi + "register.php",
-            data: {
-                "package": user.isPackage,
-                "fbToken": user.fbToken
-            },
-            dataType: "json",
-            success: function (data) {
-                if (data.status == "success") {
-                    //SUCCESS
-                    callback(true, "success");
+            user.set({
+                'package': iPackage,
+                'fbToken': sFacebookToken
+            });
+        
+            user.save(
+                {
+                    error: function (model, response) {
+                        //TODO: properly handle errors
+                        callback(false, "Ajax error: " + response.status);
+                    }
+                },
+                {
+                    success: function (model, response) {
+                        // SUCCESS
+                        if (response.status == "success") {
+                            // store the user details locally
+                            saveLocally("user", user);
+                            // callback success
+                            callback(true, "success");
+                        }
+                        // FAIL
+                        else {
+                            callback(false, response.status);
+                        }                    
+                    }
                 }
-                else {
-                    //FAIL
-                    callback(false, data.status);
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                //TODO: properly handle ajax error
-                callback(false, "Ajax error: " + errorThrown);
+            );
+        }
+
+        /**
+        * Logs the user in the system.
+        * @param {String} username
+        * @param {String} password
+        * @param {Function} [callback]
+        * @return {Object} Returns an object {'s': 'success/fail'}
+        */
+        this.login = function (sUsername, sPassword, sCallback) {
+            username = sUsername;
+            isLoggedIn = true;
+            if (sCallback) {
+                sCallback({ 's': 'success' });
+            } else {
+                return { 's': 'success' };
             }
-
-        });
-    }
-
-    /**
-    * Logs the user in the system.
-    * @param {String} username
-    * @param {String} password
-    * @param {Function} [callback]
-    * @return {Object} Returns an object {'s': 'success/fail'}
-    */
-    this.login = function (sUsername, sPassword, sCallback) {
-        username = sUsername;
-        isLoggedIn = true;
-        if (sCallback) {
-            sCallback({ 's': 'success' });
-        } else {
-            return { 's': 'success' };
         }
-    }
 
-    /**
-    * Logs the user out of the system.
-    * @param {Function} [callback]
-    * @return {Object}	Returns an object {'s': 'success/fail'}
-    */
-    this.logout = function (sCallback) {
-        isLoggedIn = false;
+        /**
+        * Logs the user out of the system.
+        * @param {Function} [callback]
+        * @return {Object}	Returns an object {'s': 'success/fail'}
+        */
+        this.logout = function (sCallback) {
+            isLoggedIn = false;
 
-        if (sCallback) {
-            sCallback({ 's': 'success' });
-        } else {
-            return { 's': 'success' };
-        }
-    }
-
-    /**
-	* Returns the login status of the user.
-	* @return {Bool}	If the user is logged-in it returns true, otherwise false.
-	*/
-    this.isLoggedIn = function () {
-        return isLoggedIn;
-
-    }
-
-    /**
-	* Returns the username of the currently logged-in user.
-	* @return {String}	The username.
-	*/
-    this.username = function () {
-        return username;
-    }
-
-    /**
-	* Returns the user's notifications.
-	* @param {Object}	[callback]
-	* @return {Object}
-	*/
-    this.getNotifications = function (sCallback) {
-        var jNotifications =
-        {
-            "notifications":
-	                  [
-            {
-                "date": "3 hours ago",
-                "title": "Jill, 29",
-                "message": "She joined Instasoda near you.",
-                "image": "/api/images/1.png",
-                "compatibility": 47
-            },
-            {
-                "date": "4 hours ago",
-                "title": "Kate, 26",
-                "message": "She checked you out a few hours ago.",
-                "image": "/api/images/2.png",
-                "compatibility": 68
-            },
-            {
-                "date": "12 hours ago",
-                "title": "Nancy, 27",
-                "message": "She wants to meet you.",
-                "image": "/api/images/3.png",
-                "compatibility": 73
-            },
-            {
-                "date": "15 hours ago",
-                "title": "Dianna, 28",
-                "message": "She joined Instasoda near you.",
-                "image": "/api/images/4.png",
-                "compatibility": 92
+            if (sCallback) {
+                sCallback({ 's': 'success' });
+            } else {
+                return { 's': 'success' };
             }
-	                  ]
-        };
-
-        if (sCallback) {
-            sCallback(jNotifications);
-        } else {
-            return jNotifications;
         }
-    }
 
-    /**
-	* Returns the user's activity feed (30 items at a time).
-	* @param (Bool)	[more]	When true, returns the next 30 items.
-	* @param {Object}	[callback]
-	* @return {Object}
-	*/
-    this.getActivityFeed = function (bMore, sCallback) {
-        activityFeedPage = 1;
-        var jActivity =
-        {
-            "recent_updates":
-	                  [
-            {
-                "date": "3 hours ago",
-                "title": "Jill, 29",
-                "message": "She joined Instasoda near you.",
-                "image": "/api/images/1.png",
-                "compatibility": 47
-            },
-            {
-                "date": "4 hours ago",
-                "title": "Kate, 26",
-                "message": "She checked you out a few hours ago.",
-                "image": "/api/images/2.png",
-                "compatibility": 68
-            },
-            {
-                "date": "12 hours ago",
-                "title": "Nancy, 27",
-                "message": "She wants to meet you.",
-                "image": "/api/images/3.png",
-                "compatibility": 73
-            },
-            {
-                "date": "15 hours ago",
-                "title": "Dianna, 28",
-                "message": "She joined Instasoda near you.",
-                "image": "/api/images/4.png",
-                "compatibility": 92
-            }
-	                  ]
-        };
+        /**
+	    * Returns the login status of the user.
+	    * @return {Bool}	If the user is logged-in it returns true, otherwise false.
+	    */
+        this.isLoggedIn = function () {
+            return isLoggedIn;
 
-        if (sCallback) {
-            sCallback(jActivity);
-        } else {
-            return jActivity;
         }
-    }
 
-    /**
-	* Returns an array of users.
-	* @param (JSON)	options	Options to customise the user array
-	* @param {Object}	[callback]
-	* @return {Object}
-	*/
-    this.getUsers = function (iNumUsers, jOptions, sCallback) {
-
-        //TODO
-
-
-        if (sCallback) {
-            sCallback(jUsers);
-        } else {
-            return jUsers;
+        /**
+	    * Returns the username of the currently logged-in user.
+	    * @return {String}	The username.
+	    */
+        this.username = function () {
+            return username;
         }
-    }
+
+   
 
 }
