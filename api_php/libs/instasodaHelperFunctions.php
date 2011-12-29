97e5b01734adc94e2524eaf0903f9c7fc9a5f388
Kostas Mavropalias <mavropalias@kostas.(none)>
2011-12-29 16:52:43 +0000

    php API helper functions

---
+++ api_php/libs/instasodaHelperFunctions.php
@@ -0,0 +1,23 @@
+<?php
+
+function fetchUrl($url)
+{
+    $curl = curl_init($url);
+    $result = false;
+    
+    if (is_resource($curl) === true)
+    {
+        curl_setopt($curl, CURLOPT_FAILONERROR, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+    }
+
+    return $result;
+}
+
+?>
