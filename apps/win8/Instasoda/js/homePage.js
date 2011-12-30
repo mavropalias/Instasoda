5226f224d23a6f06df3b61f5ca7b4831d1dd600f
Kostas Mavropalias <mavropalias@kostas.(none)>
2011-12-30 05:51:01 +0000

    code cleanup + ajax calls after login + image upload in settings

--- apps/win8/Instasoda/js/homePage.js
+++ apps/win8/Instasoda/js/homePage.js
@@ -23,143 +23,171 @@
 
 $(document).ready(function () {
 
-    $('section[role="main"]').hide();
+    // =======================================
+    // INIT APP
+    // =======================================
 
-    if (IS.login()) {
-        if (IS.accountIsComplete()) {
-            $('#dashboard').show();
+        $('section[role="main"]').hide();
+
+        if (IS.login()) {
+            if (IS.accountIsComplete()) {
+                $('#dashboard').show();
+            } else {
+                $('#facebookLoading').hide();
+                $('#facebookData').show();
+                showSettingsPage();
+            }
         } else {
-            $('#settings').show();
+            $('#registerAccount').show();
         }
-    } else {
-        $('#registerAccount').show();
-    }
 
-    //debug
-    /*   // create the user account
-        try {
-            IS.createAccount("3", "AAACRA55XRqgBAK36aOERkoK4ccnmWTwkrKPZCACNJA796rX89U5tHSiZAiJZCnJorhgBVYXGACKJgy4myByZCD2Xcc6NNUIZD", function (success, jData) {
-                if (success) {
-                    $('#fbResponse').html("success " + jData);
-                } else {
-                    $('#fbResponse').html("error: " + jData.status);
-                }
-            });
-        } catch (e) {
-            $('#fbResponse').html("error:  " + e);
-        }*/
+    // =======================================
+    // SETTINGS PAGE
+    // =======================================
 
+        function showSettingsPage() {
 
-    var iPackage = 0;
-    var scope1 = "email,user_relationships,user_location,user_hometown,user_birthday";
-    var scope2 = scope1 + ",user_activities,user_education_history,read_stream,user_interests,user_likes,user_photos";
-    var scope3 = scope2 + ",sms,offline_access,user_videos";
+            // hide other pages
+            $('section[role="main"]').hide();
 
-    $('#register1').click(function () {
-        //iPackage = 1;
-        //launchFacebookWebAuth(scope1);
-        animateContent($('#registerAccount'), $('#dashboard'))
-    });
-    $('#register2').click(function () {
-        iPackage = 2;
-        launchFacebookWebAuth(scope2);
-    });
-    $('#register3').click(function () {
-        iPackage = 3;
-        launchFacebookWebAuth(scope3);
-    });
+            // show settings
+            animateContentIn($('#settings'));
 
-    function animateContent(animateOut, animateIn) {
-        animateOut.addClass('isAnimated hasEasing isNotVisible hasNoLeftPadding')
-        setTimeout(function () {
-            animateOut.hide()
-                      .removeClass('isAnimated hasEasing isNotVisible hasNoLeftPadding')
-            animateIn.addClass('addExtraLeftPadding isNotVisible')
-                     .addClass('isAnimated hasEasing')
-                     .show()
-                     .removeClass('addExtraLeftPadding isNotVisible')
-                     .removeClass('isAnimated hasEasing')
-        }, 500)
-
-       /* if (animateAfter != false) {
-            animateAfter.hide()
-
-            setTimeout(function () {
-                animateAfter.addClass('addExtraLeftPadding isNotVisible')
-                            .addClass('isAnimated hasEasing')
-                            .show()
-                            .removeClass('addExtraLeftPadding isNotVisible')
-                            .removeClass('isAnimated hasEasing')
-            }, 1000)
-        }*/
-    }
-
-    function launchFacebookWebAuth(scope) {
-        var facebookURL = "https://www.facebook.com/dialog/oauth?client_id=";
-        var clientID = "159444727449256";
-        var callbackURL = "https://www.facebook.com/connect/login_success.html";
-        facebookURL += clientID + "&redirect_uri=" + encodeURIComponent(callbackURL) +
- "&scope=" + scope + "&display=popup&response_type=token";
-
-        try {
-            var startURI = new Windows.Foundation.Uri(facebookURL);
-            var endURI = new Windows.Foundation.Uri(callbackURL);
-
-            Windows.Security.Authentication.Web.WebAuthenticationBroker.authenticateAsync(
-			Windows.Security.Authentication.Web.WebAuthenticationOptions.default,
-			startURI,
-			endURI).then(callbackFacebookWebAuth, callbackFacebookWebAuthError);
-        }
-        catch (err) {	/*Error launching WebAuth"*/	return; }
-    }
-
-    function callbackFacebookWebAuth(result) {
-        var FacebookReturnedToken = result.responseData;
-        var response = "Status returned by WebAuth broker: " + result.responseStatus + "\r\n";
-        if (result.responseStatus == 2) {
-            response += "Error returned: " + result.responseErrorDetail + "\r\n";
-            $('#fbResponse').html("<strong>Oh snap! An error occured :( Try again please.</strong>");
-        } else if (result.responseStatus == 0) {
-
-            // success - the user has given permission and we now have a url with the token
-            // parse token value from FacebookReturnedToken
-            sToken = getParameterByName("access_token", FacebookReturnedToken);
-
-            // create the user account
-            try {
-                IS.createAccount(iPackage, sToken, function (success, jData) {
-                    if (success) {
-                        //$('#registerAccount').hide();
-                        if (IS.accountIsComplete()) {
-                            $('#dashboard').show();
-                        } else {
-                            //$('#settings').show();
-                            animateContent($('#registerAccount'), $('#settings'))
-                        }
+            // create a FileOpenPicker object for the image file picker button
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+            openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.thumbnail; //show images in thumbnail mode
+            openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.picturesLibrary; // start browsing in My Pictures library
+            openPicker.fileTypeFilter.replaceAll([".png", ".jpg", ".jpeg"]); // show only image files
+            
+            // add the event listener for the image file picker button
+            $('#addPictures').click(function () {
+                openPicker.pickSingleFileAsync().then(function (file) {
+                    // Ensure picked file is valid and usable
+                    if (file) {
+                        // append picture in the page
+                        
+                        //$('#userPictures').append('<img scr="' + file.folderRelativeId + '\\' + file.fileName + '">' + file.folderRelativeId + '\\' + file.fileName);
+                        //$('#userPictures').append('<img scr="' + URL.createObjectURL(file) + '">');
+                        $('#userPictures').append('<img height=200 src="' + URL.createObjectURL(file) + '">');
                     } else {
-                        $('#fbResponse').html("error: " + jData.status);
+                        // File not valid
+                        $('#userPictures').append("error");
                     }
                 });
-            } catch (e) {
-                $('#fbResponse').html("error:  " + e);
-            }
-
-
+            });
         }
-    }
-
-    function callbackFacebookWebAuthError(err) {
-        var error = "Error returned by WebAuth broker. Error Number: " + err.number + " Error Message: " + err.message + "\r\n";
-    }
-
-    function getParameterByName(sParam, FacebookReturnedToken) {
-
-        var match = RegExp('[#&]' + sParam + '=([^&]*)')
-                    .exec(FacebookReturnedToken);
-
-        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-
-    }
 
 
+    // =======================================
+    // REGISTER USER
+    // =======================================
+
+        var iPackage = 0;
+        var scope1 = "email,user_relationships,user_location,user_hometown,user_birthday";
+        var scope2 = scope1 + ",user_activities,user_education_history,read_stream,user_interests,user_likes,user_photos";
+        var scope3 = scope2 + ",sms,offline_access,user_videos";
+
+        $('#register1').click(function () {
+            //iPackage = 1;
+            //launchFacebookWebAuth(scope1);
+            animateContentInOut($('#registerAccount'), $('#dashboard'))
+        });
+        $('#register2').click(function () {
+            iPackage = 2;
+            launchFacebookWebAuth(scope2);
+        });
+        $('#register3').click(function () {
+            iPackage = 3;
+            launchFacebookWebAuth(scope3);
+        });
+
+        function animateContentInOut(animateOut, animateIn) {
+            animateOut.addClass('isAnimated hasEasing isNotVisible hasNoLeftPadding');
+            setTimeout(function () {
+                animateOut.hide()
+                          .removeClass('isAnimated hasEasing isNotVisible hasNoLeftPadding');
+                animateIn.addClass('addExtraLeftPadding isNotVisible')
+                         .addClass('isAnimated hasEasing')
+                         .show()
+                         .removeClass('addExtraLeftPadding isNotVisible')
+                         .removeClass('isAnimated hasEasing');
+            }, 500);
+
+           /* if (animateAfter != false) {
+                animateAfter.hide()
+
+                setTimeout(function () {
+                    animateAfter.addClass('addExtraLeftPadding isNotVisible')
+                                .addClass('isAnimated hasEasing')
+                                .show()
+                                .removeClass('addExtraLeftPadding isNotVisible')
+                                .removeClass('isAnimated hasEasing')
+                }, 1000)
+            }*/
+        }
+
+        function animateContentIn(animateIn) {
+            animateIn.addClass('addExtraLeftPadding isNotVisible')
+                        .addClass('isAnimated hasEasing')
+                        .show()
+                        .removeClass('addExtraLeftPadding isNotVisible')
+                        .removeClass('isAnimated hasEasing');
+        }
+
+        function launchFacebookWebAuth(scope) {
+            var facebookURL = "https://www.facebook.com/dialog/oauth?client_id=";
+            var clientID = "159444727449256";
+            var callbackURL = "https://www.facebook.com/connect/login_success.html";
+            facebookURL += clientID + "&redirect_uri=" + encodeURIComponent(callbackURL) + "&scope=" + scope + "&display=popup&response_type=token";
+
+            try {
+                var startURI = new Windows.Foundation.Uri(facebookURL);
+                var endURI = new Windows.Foundation.Uri(callbackURL);
+
+                Windows.Security.Authentication.Web.WebAuthenticationBroker.authenticateAsync(
+			    Windows.Security.Authentication.Web.WebAuthenticationOptions.default,
+			    startURI,
+			    endURI).then(callbackFacebookWebAuth, callbackFacebookWebAuthError);
+            }
+            catch (err) {	/*Error launching WebAuth"*/	return; }
+        }
+
+        function callbackFacebookWebAuth(result) {
+            var FacebookReturnedToken = result.responseData;
+            var response = "Status returned by WebAuth broker: " + result.responseStatus + "\r\n";
+            if (result.responseStatus == 2) {
+                response += "Error returned: " + result.responseErrorDetail + "\r\n";
+                $('#fbResponse').html("<strong>Oh snap! An error occured :( Try again please.</strong>");
+            } else if (result.responseStatus == 0) {
+
+                // success - the user has given permission and we now have a url with the token
+                // parse token value from FacebookReturnedToken
+                sToken = getParameterByName("access_token", FacebookReturnedToken);
+
+                // create the user account
+                try {
+                    IS.createAccount(iPackage, sToken, function (success, jData) {
+                        if (success) {
+                            $('#facebookLoading').fadeOut();
+                            $('#facebookData').fadeIn();
+                        } else {
+                            $('#fbResponse').html("error: " + jData.status);
+                        }
+                    });
+                    animateContentInOut($('#registerAccount'), $('#settings'));
+                } catch (e) {
+                    $('#fbResponse').html("error:  " + e);
+                }
+            }
+        }
+
+        function callbackFacebookWebAuthError(err) {
+            var error = "Error returned by WebAuth broker. Error Number: " + err.number + " Error Message: " + err.message + "\r\n";
+        }
+
+        function getParameterByName(sParam, FacebookReturnedToken) {
+            var match = RegExp('[#&]' + sParam + '=([^&]*)')
+                        .exec(FacebookReturnedToken);
+            return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+        }
 });
