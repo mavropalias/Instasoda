0e84faf21e182726c2652201137159f7ffa93c04
Kostas Mavropalias <mavropalias@kostas.(none)>
2012-01-08 06:02:15 +0000

    search results appear MUCH faster now

--- api_php/users.php
+++ api_php/users.php
@@ -13,7 +13,7 @@
         try {
             $STH = $DB->prepare('SELECT username, age, gender, interestedInMen, interestedInWomen, photos, fbThirdPartyId, aboutMe, fbUid, fbToken, fbLikesCount  FROM users');
             $STH->setFetchMode(PDO::FETCH_OBJ);
-            $STH->execute($detectId);
+            $STH->execute();
         } catch (PDOException $e) {
             header( 'HTTP/1.1 400: BAD REQUEST' );
             echo json_encode(array("status"=>"Could not read database."));
@@ -26,15 +26,26 @@
         $n = 0;
         while($row = $STH->fetch()) {
             // fetch all user's likes
-            $offset = 0;
+            /*$offset = 0;
             $fbLikesResult = new stdClass;
             do {
                 $queryLikesUrl = "https://graph.facebook.com/".$row->fbUid."/likes?format=json&limit=5000&offset=".$offset."&access_token=".$row->fbToken;
                 $queryLikesJson = json_decode(fetchUrl($queryLikesUrl));
                 $fbLikesResult = array_merge((array) $fbLikesResult, (array) $queryLikesJson->{'data'});
                 $offset = $offset + 5000;
-            } while ($queryLikesJson->{'paging'}->{'next'} != "");
-            $row->likes = $fbLikesResult;
+            } while ($queryLikesJson->{'paging'}->{'next'} != "");*/
+            try {
+                $STHLikes = $DB->prepare("SELECT likes.name, likes.id FROM likes, users_likes WHERE likes.id = users_likes.likeId AND users_likes.userId = '".$row->fbThirdPartyId."'");
+                $STHLikes->setFetchMode(PDO::FETCH_OBJ);
+                $STHLikes->execute();
+            } catch (PDOException $e) {
+                header( 'HTTP/1.1 400: BAD REQUEST' );
+                echo json_encode(array("status"=>"Could not read database2."));
+                file_put_contents('InstasodaPDOErrors.txt', $e->getMessage(), FILE_APPEND);
+                die();
+            }
+            $rowLikes = $STHLikes->fetchAll();
+            $row->likes = $rowLikes;
             
             // output user's data  
             if($n > 0) echo(',');
