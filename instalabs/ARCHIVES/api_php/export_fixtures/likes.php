212e717e3a09172c490a9bd29842ca1a40ae9352
Kostas Mavropalias <mavropalias@gmail.com>
2012-02-27 02:48:48 +0000

    exports 'likes' table as data fixture for MongoDB

---
+++ instalabs/ARCHIVES/api_php/export_fixtures/likes.php
@@ -0,0 +1,44 @@
+<?php 
+    
+    require '../config.php';    
+    
+    // ========================================
+    // ========================================
+    // AUTHORIZATION
+    // ========================================
+    // ========================================
+
+        $sApiSecretKey = "aG35svDHJURCG35253dCFDC69fvsf3fhg0f";
+        if($_GET['skey'] != $sApiSecretKey) die("Authorization failed - ip/datetime logged.");
+    
+    // ======================================== 
+    // ========================================
+    // READ (GET)
+    // ========================================
+    // ========================================
+    if($_SERVER['REQUEST_METHOD'] == 'GET' || true) {
+        try {
+            $STH = $DB->prepare('SELECT id AS i, name AS n, category AS c FROM likes');
+            $STH->setFetchMode(PDO::FETCH_OBJ);
+            $STH->execute();
+        } catch (PDOException $e) {
+            header( 'HTTP/1.1 400: BAD REQUEST' );
+            echo json_encode(array("status"=>"Could not read database. ".$e));
+            file_put_contents('InstasodaPDOErrors.txt', $e->getMessage(), FILE_APPEND);
+            die();
+        }
+
+        header('HTTP/1.1 200 OK');
+        echo ('[');
+        $n = 0;
+        while($row = $STH->fetch()) {
+
+            if($n > 0) echo(',');
+            echo json_encode($row);
+            
+            // increment counter
+            $n++;            
+        }
+        echo (']');    
+    }   
+?>
