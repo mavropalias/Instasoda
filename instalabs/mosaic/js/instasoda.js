bc8967d8b3edf8ddfb090d6a5ed662fe32afdd0b
Konstantinos Vasileiou <k.vassiliou@gmail.com>
2012-04-03 07:12:23 +0300

    [WIP] Use Disqus for comments

--- instalabs/mosaic/js/instasoda.js
+++ instalabs/mosaic/js/instasoda.js
@@ -11,6 +11,14 @@ $(document).ready(function(){
     jQuery.support.cors = true;
     Backbone.emulateHTTP = true;
 
+    // DISQUS settings
+    disqus_developer = 1;
+    var papi_key = 'gFHfw6LElOAHK7e862NuDxdQl3shnEpcJB1BprqwiBf75n41BbdUtFtV8c5GW94S',
+        sapi_key = 'qFNSRuuzMCwIkcSjW8OlKX3hRAOflg86WvoVYbmohyQnFGhMoRfz2Tv52xEKWYBZ',
+        _disqus_url = "http://localhost/Instasoda/instalabs/mosaic/",
+        _disqus_story = "#!/story/",
+        disqus_shortname = 'mosaicdev';
+
 
 
     // ===================================================
@@ -56,12 +64,62 @@ $(document).ready(function(){
       tagName: 'article',
       className: 'item',
       events: {
-        'click':'maximizeStory'
+        'click':'maximizeStory',
+        'mouseenter': 'mouseoverstory',
+        'mouseleave' : 'mouseleavestory'
+      },
+
+      mouseoverstory: function(e) {
+        var _storyID = this.model.get('id');
+        var _storyOffset = this.$el.offset();
+        _currentStoryElement = "#" + _storyID + 'tooltip';
+        _currentStoryElementSection = _currentStoryElement + '> section';
+
+        if ( $(_currentStoryElement).is('.loadedComment') ) {
+          $(_currentStoryElement).css({left:_storyOffset.left - 5},{top:_storyOffset.top}).slideDown(300).addClass('loadedComment');
+        } else {
+          _tooltipLoader = this.$el.find('img');
+          _tooltipLoader.delay(500).fadeIn(500);
+          tooltipFetchInfo = setTimeout(function() {
+            _DISQUS(_storyID, function(data) {
+              $(_currentStoryElement).css({left:_storyOffset.left - 5},{top:_storyOffset.top}).slideDown(300).addClass('loadedComment');
+
+              console.log(data);
+              $.each(data, function() {
+                var $i = 0;
+                $.each(this, function(k, v) {
+                  $(_currentStoryElementSection).append(data.response[$i].author.name + ' said: ' + data.response[$i].message);
+                  $i++;
+                });
+              });
+
+              //$(_currentStoryElementSection).append(data.response[0].author.name);
+              _tooltipLoader.hide();
+            });
+          }, 2000);
+        }
+      },
+
+      mouseleavestory: function(e) {
+        clearTimeout(tooltipFetchInfo);
+        _tooltipLoader.hide();
+        setTimeout(function() {
+          $('.commentTooltip').fadeOut(300);
+        }, 1500);
       },
 
       maximizeStory: function (e) {
         console.log('> Navigate to the [Full story]');
-        mosaicRouter.navigate("story/" + this.model.get('id'), {trigger: true, replace: false});
+        mosaicRouter.navigate("!/story/" + this.model.get('id'), {trigger: true, replace: false});
+      },
+
+      renderComments: function(model) {
+        _DISQUScomments(model.get('id'), function(data) {
+          model.set({comments: data});
+          $('footer > #' + model.get('id'))
+            .text(data)
+            .css({opacity:1});
+        });
       },
 
       render: function () {
@@ -89,6 +147,7 @@ $(document).ready(function(){
 
       renderStory: function (model) {
         var storyView = new StoryView({ model: model });
+        storyView.renderComments(model);
         storyView.render();
         $("#container").append(storyView.el);
       },
@@ -123,7 +182,7 @@ $(document).ready(function(){
             console.log('   ~~~ SUCCESS: Added a new story to database!');
             storiesCollection.add(story);
             console.log('  > Navigate to the new [Full story]');
-            mosaicRouter.navigate("story/" + story.id, {trigger: true, replace: true});
+            mosaicRouter.navigate("!/story/" + story.id, {trigger: true, replace: true});
           },
           error: function (model, response) {
             console.log('   !!! ERROR: Could not add the new story to database!');
@@ -159,20 +218,20 @@ $(document).ready(function(){
       save: function() {
         console.log('  ~ Preparing to add a new [Comment] to the story with ID: ' + story.id);
         var comment = new Comment();
-        commentText = $('textarea[name=commenttext]').val();
-        commentAuthor = story.get('author');
+        //commentText = $('textarea[name=commenttext]').val();
+        //commentAuthor = story.get('author');
 
         comment.save(
         {
           'postId':story.id,
-          'author':commentAuthor,
-          'content':commentText
+          'author':'someone',
+          'content':'something'
         },
         {
           success: function (model, response){
-            $('textarea[name=commenttext]').val('');
+            //$('textarea[name=commenttext]').val('');
             console.log('   ~~~ SUCCESS: Added the new [Comment] to database');
-            processComment();
+            //processComment();
           },
           error: function (model, response) {
             console.log('   !!! ERROR: Could not add [Comment] to database');
@@ -229,7 +288,7 @@ $(document).ready(function(){
         "/": "index",
 
         // Mosaic Full story
-        "story/:id": "fullStory"
+        "!/story/:id": "fullStory"
       },
 
       initialize: function(){
@@ -260,6 +319,7 @@ $(document).ready(function(){
             $("body").append(storyFullView.el);
             $("#articleFullView").show();
             $("body").css({'overflow':'hidden'});
+            _DISQUSappend(story.get('id'));
           }
         });
       }
@@ -292,5 +352,65 @@ $(document).ready(function(){
       $(this).slideDown(300);
     })
   }
+
+  function _DISQUSappend(id) {
+    var disqus_identifier = id;
+    var disqus_url = _disqus_url + _disqus_story + id;
+    console.log(' /// ' + disqus_url + ' /// ' + disqus_shortname);
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  }
+
+  function _DISQUScomments(id, cb) {
+    // Get a story's comments
+    $.ajax({
+      url: 'https://disqus.com/api/3.0/threads/list.json',
+      data: {
+          'api_key': papi_key,
+          'forum': disqus_shortname,
+          'thread': 'link:' + _disqus_url + _disqus_story + id
+      },
+      success: function(data) {
+        cb(data.response[0].posts);
+      }
+    });
+  }
+
+  function _DISQUS(id,cb) {
+    //Get all comments
+    var _storyID = id;
+    $.ajax({
+      url: 'https://disqus.com/api/3.0/threads/list.json',
+      data: {
+          'api_key': papi_key,
+          'forum': disqus_shortname,
+          'thread': 'link:' + _disqus_url + _disqus_story + _storyID
+      },
+      success: function(data) {
+        var _threadID = data.response[0].id;
+        $.ajax({
+          url: 'https://disqus.com/api/3.0/threads/listPosts.json',
+          data: {
+              'api_key': papi_key,
+              'thread': _threadID
+          },
+          success: function(data) {
+            cb(data);
+            console.log(data);
+          }
+        });
+
+      }
+    });
+  }
+
+  /*
+  disqus_config = function() {
+    this.callbacks.onNewComment.push(function() { storyFullView.save(); });
+    Available callbacks are afterRender, onInit, onNewComment, onPaginate, onReady, preData, preInit, preReset
+  }
+  */
+
 
 });
