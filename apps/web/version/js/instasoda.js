87033f4523a3b3d6aa56ff70ac6d52411ec9c967
Kostas Mavropalias <mavropalias@gmail.com>
2012-05-31 17:10:15 +0100

    WIP: notification system

--- apps/web/version/js/instasoda.js
+++ apps/web/version/js/instasoda.js
@@ -1741,6 +1741,21 @@ $(document).ready(function() {
     // -------------------------------------------------------------------------
     socket.on('connected', function (data) {
       console.log('- SOCKET.IO status: ' + data.status);
+      
+      // get notifications
+      setInterval(function() {
+        // only if user is logged-in
+        if(typeof user.get('_id') != 'undefined') {
+          socket.emit('getNotifications', {
+            _id: user.get('_id'),
+            fTkn: user.get('fTkn')
+          }, function(data) {
+            // TODO
+          });
+        } else {
+          // do nothing
+        }
+      }, 1500);
     });
     
     // Receive online users
