4f0c9a5d3f8f5bf9b640e9494955b9e7b4c95c9c
Kostas Mavropalias <mavropalias@gmail.com>
2012-06-22 01:16:52 +0100

    the user can now receive push notifications for chat messages

--- apps/web/version/js/modules/socketio.js
+++ apps/web/version/js/modules/socketio.js
@@ -18,8 +18,36 @@ socket.on('connected', function (data) {
       socket.emit('getNotifications', {
         _id: user.get('_id'),
         fTkn: user.get('fTkn')
-      }, function(data) {
-        // TODO
+      }, function(err, data) {
+        if(!IS.nullOrEmpty(data)) {
+          // check for new chat messages
+          if(!IS.nullOrEmpty(data.chat)) {
+            //console.log(chatSessions.get(data.chat[0].sId));
+
+            // loop through chat array and update chatSessions' models
+            var newMessages = data.chat;
+
+            for(var i = 0; i < newMessages.length; i++) {
+              var model = chatSessions.find( function(session) {
+                return session.get('_id') == newMessages[i].sId;
+              });
+
+              // construct message
+              var sMsg = {
+                u: newMessages[i].u,
+                t: newMessages[i].t,
+                m: newMessages[i].m
+              };
+
+              // update the model
+              model.get('log').push(sMsg);
+              model.trigger('change:log', sMsg);
+            }
+
+            
+            
+          }
+        }
       });
     } // else do nothing
   }, 2000);
@@ -29,4 +57,10 @@ socket.on('connected', function (data) {
 // -------------------------------------------------------------------------
 socket.on('onlineUsers', function (msg) {
   onlineUsers.set({ count: msg.count });
+});
+
+// Receive chat message
+// -------------------------------------------------------------------------
+socket.on('newChatMessage', function (msg) {
+  //TODO handle new message
 });
