704786b4f92e601ad5edf1b89b018007229732dc
Kostas Mavropalias <mavropalias@gmail.com>
2012-06-22 01:43:22 +0100

    socket.io now handles push chat notifications and updates the app

--- apps/web/version/js/modules/socketio.js
+++ apps/web/version/js/modules/socketio.js
@@ -20,28 +20,36 @@ socket.on('connected', function (data) {
         fTkn: user.get('fTkn')
       }, function(err, data) {
         if(!IS.nullOrEmpty(data)) {
+          
           // check for new chat messages
+          // ===========================
           if(!IS.nullOrEmpty(data.chat)) {
-            //console.log(chatSessions.get(data.chat[0].sId));
-
-            // loop through chat array and update chatSessions' models
             var newMessages = data.chat;
 
+            // loop through chat array and update chatSessions' models
             for(var i = 0; i < newMessages.length; i++) {
+
+              // detect if chat session already exists
               var model = chatSessions.find( function(session) {
                 return session.get('_id') == newMessages[i].sId;
               });
 
-              // construct message
-              var sMsg = {
-                u: newMessages[i].u,
-                t: newMessages[i].t,
-                m: newMessages[i].m
-              };
+              if(!IS.nullOrEmpty(model)) {
 
-              // update the model
-              model.get('log').push(sMsg);
-              model.trigger('change:log', sMsg);
+                // construct message
+                var sMsg = {
+                  u: newMessages[i].u,
+                  t: newMessages[i].t,
+                  m: newMessages[i].m
+                };
+
+                // update the model
+                model.get('log').push(sMsg);
+                model.trigger('change:log', sMsg);
+
+              } else {
+                // chat session is new
+              }
             }
 
             
