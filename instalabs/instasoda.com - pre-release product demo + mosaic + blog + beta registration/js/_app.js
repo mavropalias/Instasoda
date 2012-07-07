7c2b467f0a8eadff4d7e10fd251d4de141fca0e9
Kostas Mavropalias <mavropalias@gmail.com>
2012-07-07 19:18:43 +0100

    - updated open graph tags - added favicon - fixed IE9 issues - various UI tweaks

--- instalabs/instasoda.com - pre-release product demo + mosaic + blog + beta registration/js/_app.js
+++ instalabs/instasoda.com - pre-release product demo + mosaic + blog + beta registration/js/_app.js
@@ -1,7 +1,7 @@
 // Config
 // =============================================================================
   
-  var sApi = "http://localhost:8080/api/user/"; //##apiUrl## - 23.23.87.218
+  var sApi = "http://api.instasoda.com:8080/api/user/"; //##apiUrl## 
   jQuery.support.cors = true;
 
 // Create the IS namespace
@@ -11,14 +11,15 @@
 
 
   IS.facebookAuth = function(){
+    //console.log('> facebookAuth');
     var self = this;
 
     FB.getLoginStatus(function(res) {
+      //console.log('> getLoginStatus');
       var button = document.getElementById('fb-auth');
       if (!res.authResponse) {
-        //user is not connected to the app or logged out
-        //button.innerHTML = 'Login with Facebook';
-        
+        //console.log('> user is not connected to the app or logged out');
+        //user is not connected to the app or logged out        
           FB.login(function(response) { 
             // FB.Event.subscribe('auth.statusChange') will take care of the rest
             self.facebookAuth();
@@ -48,12 +49,12 @@
                 query: 'SELECT third_party_id FROM user WHERE uid=me()'
               },
               function(res) {
-                console.log('> Attempting to login the user');
+                //console.log('> Attempting to login the user');
 
                 // register for beta
                 IS.createAccount(fbToken, res[0].third_party_id, function (err, res) {
                   if(!err) {
-                    console.log('> SUCCESS! Account created');
+                    //console.log('> SUCCESS! Account created');
                     alert("thanks!");
                   } else {
                     console.log('> ERROR: ' + res);
@@ -67,67 +68,52 @@
             FB.logout();
           }
         } else if (res.status === 'not_authorized') {
-          console.log('> User is logged into Facebook but has not authenticated the application');
-          //IS.navigateTo('');
+          //console.log('> User is logged into Facebook but has not authenticated the application');
         } else {
-          console.log('> User is not logged into Facebook at this time');
-          //IS.navigateTo('');
+          //console.log('> User is not logged into Facebook at this time');
         }
       }
     });  
   }
 
 IS.createAccount = function(fbToken, fTid, cb) {
+  //console.log('> createAccount');
 
-  var register = $.post(sApi, {
-    '_id': null,
-    'fTid': fTid,
-    'fTkn': fbToken
-  })
-  .success(function() { })
-  .error(function() {
-    $('#signupStatus .status').text("OH NO! An error occured :( Please try again!");
-  })
-  .complete(function() { 
-    $('#signupStatus .status').text("SUCCESS!");
-  });
+  if ($.browser.msie && window.XDomainRequest) { // IE
+    var xdr = new XDomainRequest();
+    var data = '?fTid=' + fTid + '&fTkn=' + fbToken;
 
-  //console.log('- creating account');
-  /*user.set({
-    '_id': null,
-    'fTid': fTid,
-    'fTkn': fbToken
-  });
-
-  user.save({
-    error: function(model, response) {
-      //TODO: properly handle errors
-      //a false might only mean that the API server is N/A
-      console.log('- login error: ' + response.error);
-      cb(true, response.error);
-    }
-  }, {
-    success: function(model, response) {
-      // Ajax call was successful
-      // ------------------------
-      console.log('- got an API response');
-      // Now check if the account was created
-      // ------------------------------------
-      if ((typeof model.attributes._id !== 'undefined') && (typeof response.error === 'undefined')) {                    
-        // store the user details locally
-        // ------------------------------
-        store.set("user", user);
-
-        // callback success
-        // ----------------
-        cb(null, "success");
-      }
-      // FAIL
-      // ----
-      else {
-        console.log('- ' + response.error);
-        cb(true, response.error);
-      }
-    }
-  });*/
+    xdr.open("get", sApi + data);
+    xdr.send();
+    xdr.onload = function() {
+      $('#signupStatus > .message > .working, #signupStatus .status').fadeOut(400, function() {
+        $('#signupStatus .status').text("SUCCESS!");
+        $('#signupStatus > .message > .success, #signupStatus .status').fadeIn();
+      });
+    };
+    xdr.onsuccess = function() {
+      //alert('success');
+    };
+    xdr.onerror = function() {
+      $('#signupStatus .status').text("OH NO! An error occured :( Please try again!");
+      console.log(xdr.responseText);
+    };
+  } else { // other browsers
+    var register = $.post(sApi, {
+      '_id': null,
+      'fTid': fTid,
+      'fTkn': fbToken
+    })
+    .success(function() {
+      $('#signupStatus > .message > .working, #signupStatus .status').fadeOut(400, function() {
+        $('#signupStatus .status').text("SUCCESS!");
+        $('#signupStatus > .message > .success, #signupStatus .status').fadeIn();
+      });
+    })
+    .error(function(a,b,c) {
+      $('#signupStatus .status').text("OH NO! An error occured :( Please try again!");
+    })
+    .complete(function() { 
+    });
+  }
 }
